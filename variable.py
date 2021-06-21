8945c7331df8e88e1974d4a67a5daa2c716a2176
A-s-t-h-a <79190491+A-s-t-h-a@users.noreply.github.com>
2021-06-21 21:16:26 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,18 @@
+a=2
+b=6
+c=8
+print(a+b+c)
+type(a)
+print (type(a))
+print("bcduggfk"+"dfevdhyb")
+_name="astha"
+print(type(_name))
+v=k=l=20
+print(k)
+print(l)
+x=10
+x==10
+y=16
+print(x==10 and y!=16)
+print(x!=10 and y==16)
+
