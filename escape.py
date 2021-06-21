babf42b8ff6859046d88c331bfb7033b849c22fa
A-s-t-h-a <79190491+A-s-t-h-a@users.noreply.github.com>
2021-06-21 21:15:13 +0530

    Add files via upload

---
+++ escape.py
@@ -0,0 +1,8 @@
+print("hello world \"astha\" here") 
+print("line A\nline B")
+print ("line A\tline B")
+print("This is backslash/")
+print("this is backslash\\")
+print("this is backslash/")
+print("This is backlash /")
+print("There is hope in ever\brything")
