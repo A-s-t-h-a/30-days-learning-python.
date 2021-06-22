0cf0573780a3a201b5d2aff38c0f1fa290444bda
A-s-t-h-a <79190491+A-s-t-h-a@users.noreply.github.com>
2021-06-22 22:08:35 +0530

    Add files via upload

---
+++ literals.py
@@ -0,0 +1,12 @@
+#using operators
+#using right shift operator
+print(id(1)) # id is use demo
+print(type(1)) #finding type of 1 clas integer
+print(1 is not '1')# identify operator
+print(1 is '1')# identify operator
+nums =[1,2,3,4]# memebership operator
+print(nums)
+print(8 in nums)
+nums.append(8)
+print(nums)
+print(8 is nums)
