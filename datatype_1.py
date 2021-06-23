0acbd9041fc062e4e58a72650a5ff71095dba6c7
A-s-t-h-a <79190491+A-s-t-h-a@users.noreply.github.com>
2021-06-23 22:36:56 +0530

    Add files via upload

---
+++ datatype_1.py
@@ -0,0 +1,12 @@
+# Dop denote the type of datatype
+num = 123
+rate = 34.87868
+com =6j
+print(type(num))
+print(type(rate))
+print(type(com))
+# some string operations
+string ="abcdefg"
+# command for acessing single string
+print(string[0])
+print(string[1:3])
