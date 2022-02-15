736373fbcdf4e2495514e80ac0316f9100d15dc4
Astha Kumari <79190491+A-s-t-h-a@users.noreply.github.com>
2022-02-15 14:00:48 +0530

    Add files via upload
    Adding variable in the character line.

---
+++ data_and_variable.py
@@ -0,0 +1,4 @@
+charachter_name="Jhon"
+charachter_age="35"
+print("There used a live a man named "+charachter_name+",in the old village of Hemshire.")
+print("He was "+charachter_age+",years old.")
