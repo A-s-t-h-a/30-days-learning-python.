3b51368b5dac9d64d56b2c3dab0f318534f3a48a
Astha Kumari <79190491+A-s-t-h-a@users.noreply.github.com>
2022-02-21 08:43:41 +0530

    Add files via upload
    Code comprising of operations on the number.

---
+++ number.py
@@ -0,0 +1,24 @@
+from math import *
+print(2)
+my_name = 2
+print(my_name)
+print(3*5)
+print(3%10)
+my_name2 = 56
+print(type(str(my_name2)))
+print(abs(34.4737863))
+print(ceil(45.6272))
+print(floor(2425.63628))
+print(max(2,3))
+print(min(25,86896))
+num = 10
+
+# check and print type of num variable
+print(type(num))
+
+# convert the num into string
+converted_num = str(num)
+
+# check and print type converted_num variable
+print(type(converted_num))
+
